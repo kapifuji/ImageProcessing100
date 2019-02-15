de3ad19944e313b86bbc561fee12c07731b316bf
rpianna <kantarui619816@gmail.com>
2019-02-15 17:27:12 +0900

    q7テスト追加

---
+++ tests/test_q7.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pytest
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent.parent / "source"))
+import source.q7 as q7
+
+
+@pytest.fixture()
+def in_img():
+    in_img = np.zeros((24, 24, 3), np.uint8)
+    in_img[0:8:2, 0:8:2, ::] = 60
+    in_img[16:24:2, 16:24:2, 2] = 70
+
+    return in_img
+
+
+@pytest.fixture()
+def out_img():
+    out_img = np.zeros((24, 24, 3), np.uint8)
+    out_img[0:8, 0:8, ::] = 15
+    out_img[16:24, 16:24, 2] = 17
+
+    return out_img
+
+
+def test_apply_mean_pooling(in_img, out_img):
+    assert (q7.apply_mean_pooling(in_img) == out_img).all()
