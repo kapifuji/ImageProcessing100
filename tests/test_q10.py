8eb14affaded80c7ad76ed625b8d705cc011a988
rpianna <kantarui619816@gmail.com>
2019-02-22 02:44:54 +0900

    q10テスト完成

---
+++ tests/test_q10.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pytest
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent.parent / "source"))
+import source.q10 as q10
+
+
+def test_apply_median_filter(mocker):
+    in_img = np.zeros((3, 3, 3), np.uint8)
+
+    add_pad_img = np.zeros((5, 5, 3), np.uint8)
+    add_pad_mock = mocker.Mock(return_value=add_pad_img)
+    mocker.patch.object(q10.q9, "add_padding", add_pad_mock)
+
+    get_fil_mock = mocker.Mock(return_value=1)
+    mocker.patch.object(q10.np, "median", get_fil_mock)
+
+    del_pad_img = np.zeros((3, 3, 3), np.uint8)
+    del_pad_img[:, :, :] = 1
+    del_pad_mock = mocker.Mock(return_value=del_pad_img)    
+    mocker.patch.object(q10.q9, "delete_padding", del_pad_mock)
+
+    out_img = np.zeros((3, 3, 3), np.uint8)
+    out_img[:, :, :] = 1
+    assert (q10.apply_median_filter(in_img, 3) == out_img).all()
+
+    assert add_pad_mock.call_count == 1
+    assert (add_pad_mock.call_args_list[0][0] == in_img).all()
+
+    del_pad_arg = np.zeros((5, 5, 3), np.uint8)
+    del_pad_arg[1:4, 1:4, :] = 1
+    assert del_pad_mock.call_count == 1
+    assert (del_pad_mock.call_args_list[0][0] == del_pad_arg).all()
