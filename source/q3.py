9e0557ccb0caa2c2dea4190ca3570b577701165c
rpianna <kantarui619816@gmail.com>
2019-02-25 18:56:44 +0900

    q3のdocstringを記述

--- source/q3.py
+++ source/q3.py
@@ -3,7 +3,20 @@ import numpy as np
 import q2
 
 
-def conv_BGR2binary(img, threshold):
+def conv_BGR2binary(img, threshold: int):
+    """BGR画像をしきい値に応じて2値画像に変換します。
+
+    Arguments:
+        img {numpy.ndarray} -- BGR画像
+        threshold {[type]} -- しきい値（0～255）
+
+    Returns:
+        numpy.ndarray -- 2値画像（1ch）
+
+    Notes:
+        ndarrayのdtypeは入力画像と同じものを返します。
+    """
+
     gray_img = q2.conv_BGR2gray(img)
 
     gray_img[gray_img < threshold] = 0
