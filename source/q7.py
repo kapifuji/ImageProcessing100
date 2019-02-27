764dad1c4d639d576d598a173b71d83294d2bfaf
rpianna <kantarui619816@gmail.com>
2019-02-27 14:17:31 +0900

    q7のdocstringを記述

--- source/q7.py
+++ source/q7.py
@@ -3,6 +3,18 @@ import numpy as np
 
 
 def apply_mean_pooling_8x8(bgr_img):
+    """8x8にグリッド分割して平均プーリングします。
+
+    Arguments:
+        bgr_img {numpy.ndarray} -- BGR画像（3ch）
+
+    Returns:
+        numpy.ndarray -- 処理後のBGR画像（3ch）
+
+    Notes:
+        入力はRGB画像でも正常に動作します。
+    """
+
     out_img = bgr_img.copy()
     for h in range(0, out_img.shape[0] // 8):
         for w in range(0, out_img.shape[1] // 8):
