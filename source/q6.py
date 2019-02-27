ee21ff9824a909c4ee11fdb1778a5c528bbeeb76
rpianna <kantarui619816@gmail.com>
2019-02-27 14:14:28 +0900

    q6のdocstringを記述

--- source/q6.py
+++ source/q6.py
@@ -3,6 +3,18 @@ import numpy as np
 
 
 def apply_color_reduction(bgr_img):
+    """減色処理を適用します。
+
+    Arguments:
+        bgr_img {numpy.ndarray} -- BGR画像（3ch）
+
+    Returns:
+        numpy.ndarray -- 処理後のBGR画像（3ch）
+
+    Notes:
+        入力はRGB画像でも正常に動作します。
+    """
+
     out_img = bgr_img.copy()
     out_img[(0 <= out_img) & (out_img < 63)] = 32
     out_img[(63 <= out_img) & (out_img < 127)] = 96
